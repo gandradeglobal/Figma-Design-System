57f83d5bba8e2606f4be0056164d1e49526ac008
Goncalo Andrade <106152131+gandradeglobal@users.noreply.github.com>
2023-04-05 09:57:57 +0100

    Create file: "transformations/fds-web/icons/arrowdown-thickstroke-on.js"

---
+++ transformations/fds-web/icons/arrowdown-thickstroke-on.js
@@ -0,0 +1,4 @@
+export default `<svg width="60" height="61" viewBox="0 0 60 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M30 13.625V47.375M30 47.375L43.5 33.875M30 47.375L16.5 33.875" stroke="#353739" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
