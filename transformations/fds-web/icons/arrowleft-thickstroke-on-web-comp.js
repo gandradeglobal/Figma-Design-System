0cecb8ba1f4de434a00caac100ede9b17719e5a7
Goncalo Andrade <106152131+gandradeglobal@users.noreply.github.com>
2023-04-05 09:58:04 +0100

    Create file: "transformations/fds-web/icons/arrowleft-thickstroke-on-web-comp.js"

---
+++ transformations/fds-web/icons/arrowleft-thickstroke-on-web-comp.js
@@ -0,0 +1,39 @@
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host {
+      width: 42px;
+      height: 42px;
+    }
+      :host, #root {
+        display: inline-block;
+      }
+        #root, svg {
+          width: 100%;
+          height: 100%;
+        }
+          svg, svg * {
+            fill: var( --color, #222 ) !important;
+          }
+      .error {
+        font-family: sans-serif;
+        font-size: 12px;
+        color: #f00;
+      }
+  </style>
+  <div id="root"><svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M46.875 29.9999H13.125M13.125 29.9999L26.625 43.5M13.125 29.9999L26.625 16.4999" stroke="#353739" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+</div>
+`;
+
+class LIBarrowleftThickstrokeOn extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+window.customElements.define('lib-icon-arrowleft-thickstroke-on', LIBarrowleftThickstrokeOn);
